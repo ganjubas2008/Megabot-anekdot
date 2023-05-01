5aa9480ee2a8ebfbba2be9bcc41d6f1159b2e9c4
andrei.sadchikov <sadchikov.ae@phystech.edu>
2023-05-01 17:51:45 +0300

    minor changes

--- app.py
+++ app.py
@@ -6,23 +6,29 @@ import random
 from jokeclass import Joke
 
 import db
+import os
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start'])
+if not os.path.exists("data"):
+    os.mkdir("data")
+
+
+@bot.message_handler(commands=["start"])
 def welcome(message):
-    
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     b1 = types.KeyboardButton("расскажи хороший анекдот")
     b2 = types.KeyboardButton("расскажи обычный анекдот")
     b3 = types.KeyboardButton("расскажи анекдот, у которого много 👍")
     b4 = types.KeyboardButton("расскажи анекдот, у которого много 👎")
-    
+
     markup.add(b1, b2, b3, b4)
-    
+
     bot.send_message(message.chat.id, "здравствуйте", reply_markup=markup)
-    
-@bot.message_handler(content_types=['text'])
+
+
+@bot.message_handler(content_types=["text"])
 def response(message):
     markup = types.InlineKeyboardMarkup(row_width=2)
 
@@ -34,54 +40,53 @@ def response(message):
     ]:
         bot.send_message(message.chat.id, "неправильная команда")
         return
-    
+
     joke = Joke()
-    if (message.text == "расскажи хороший анекдот"):
+    if message.text == "расскажи хороший анекдот":
         joke = parsing.get_good(random.randint(1, 30))
-        
-    elif (message.text == "расскажи обычный анекдот"):
+
+    elif message.text == "расскажи обычный анекдот":
         joke = parsing.get_any(random.randint(1, 1100))
-        
-    elif (message.text == "расскажи анекдот, у которого много 👍"):
+
+    elif message.text == "расскажи анекдот, у которого много 👍":
         joke = db.get_best()
-        
-    elif (message.text == "расскажи анекдот, у которого много 👎"):
+
+    elif message.text == "расскажи анекдот, у которого много 👎":
         joke = db.get_worst()
-        
-    item1 = types.InlineKeyboardButton("👍", callback_data=f'like{joke.id}')
-    item2 = types.InlineKeyboardButton("👎", callback_data=f'dislike{joke.id}')
+
+    item1 = types.InlineKeyboardButton("👍", callback_data=f"like{joke.id}")
+    item2 = types.InlineKeyboardButton("👎", callback_data=f"dislike{joke.id}")
     markup.add(item1, item2)
     bot.send_message(message.chat.id, str(joke), reply_markup=markup)
-        
-        
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     try:
         if call.message:
             id = 0
             delta = (0, 0)
-            if call.data[:4] == 'like':
+            if call.data[:4] == "like":
                 id = int(call.data[4:])
                 delta = (1, 0)
-                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
-                
-            elif call.data[:7] == 'dislike':
-                bot.send_message(call.message.chat.id, 'Бывает 😢')
+                bot.send_message(call.message.chat.id, "Вот и отличненько 😊")
+
+            elif call.data[:7] == "dislike":
+                bot.send_message(call.message.chat.id, "Бывает 😢")
                 id = int(call.data[7:])
                 delta = (0, 1)
-                bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
-                    text="не нравится - уходи")
-                
- 
+
             # Remove inline buttons and update likes counter
             db.update(id=id, delta=delta)
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=str(db.get(id)),
-                    reply_markup=None)
- 
-            
- 
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                text=str(db.get(id)),
+                reply_markup=None,
+            )
+
     except Exception as e:
         print(repr(e))
-    
+
 
 bot.polling(none_stop=True)
--- db.py
+++ db.py
@@ -4,13 +4,15 @@ from jokeclass import Joke
 
 def get(id, basename="mytable"):
     # Connect to the database
-    conn = sqlite3.connect('data/jokes.db')
-    
-    conn.execute(f'''CREATE TABLE IF NOT EXISTS {basename}
+    conn = sqlite3.connect("data/jokes.db")
+
+    conn.execute(
+        f"""CREATE TABLE IF NOT EXISTS {basename}
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
               text TEXT NOT NULL,
               likes INTEGER KEY,
-              dislikes INTEGER KEY);''')
+              dislikes INTEGER KEY);"""
+    )
 
     # Retrieve data from the table
     cursor = conn.execute(f"SELECT text FROM {basename} WHERE id=?", (id,))
@@ -21,9 +23,14 @@ def get(id, basename="mytable"):
         cursor = conn.execute(f"SELECT likes FROM {basename} WHERE id=?", (id,))
         likes_res = cursor.fetchone()
         cursor = conn.execute(f"SELECT dislikes FROM {basename} WHERE id=?", (id,))
-        dislikes_res = cursor.fetchone()    
+        dislikes_res = cursor.fetchone()
         conn.close()
-        return Joke(id=id, text=text_res[0], likes=int(likes_res[0]), dislikes=int(dislikes_res[0]))
+        return Joke(
+            id=id,
+            text=text_res[0],
+            likes=int(likes_res[0]),
+            dislikes=int(dislikes_res[0]),
+        )
     else:
         conn.close()
         return Joke()
@@ -34,17 +41,19 @@ def add(joke, basename="mytable"):
     if joke.id == 0:
         return
     # Connect to the database
-    conn = sqlite3.connect('data/jokes.db')
+    conn = sqlite3.connect("data/jokes.db")
 
-    conn.execute(f'''CREATE TABLE IF NOT EXISTS {basename}
+    conn.execute(
+        f"""CREATE TABLE IF NOT EXISTS {basename}
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
               text TEXT NOT NULL,
               likes INTEGER KEY,
-              dislikes INTEGER KEY);''')
-    
+              dislikes INTEGER KEY);"""
+    )
+
     # Delete if joke is in the database
     old_joke = get(joke.id)
-    if old_joke.id != 0: #if id is present in database
+    if old_joke.id != 0:  # if id is present in database
         if old_joke == joke:
             return
         else:
@@ -53,26 +62,33 @@ def add(joke, basename="mytable"):
             conn.commit()
 
     # Insert data into the table
-    conn.execute(f"INSERT INTO {basename} (id, text, likes, dislikes) VALUES (?, ?, ?, ?)", (joke.id, joke.text, joke.likes, joke.dislikes))
+    conn.execute(
+        f"INSERT INTO {basename} (id, text, likes, dislikes) VALUES (?, ?, ?, ?)",
+        (joke.id, joke.text, joke.likes, joke.dislikes),
+    )
     conn.commit()
 
     # Close the connection
     conn.close()
 
+
 def clear_database(basename="mytable"):
-    conn = sqlite3.connect('data/jokes.db')
+    conn = sqlite3.connect("data/jokes.db")
     c = conn.cursor()
     c.execute(f"DROP TABLE IF EXISTS {basename}")
-    conn.execute(f'''CREATE TABLE IF NOT EXISTS {basename}
+    conn.execute(
+        f"""CREATE TABLE IF NOT EXISTS {basename}
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
               text TEXT NOT NULL,
               likes INTEGER KEY,
-              dislikes INTEGER KEY);''')
+              dislikes INTEGER KEY);"""
+    )
     conn.commit()
     conn.close()
-    
+
+
 def get_best(basename="mytable"):
-    conn = sqlite3.connect('data/jokes.db')
+    conn = sqlite3.connect("data/jokes.db")
 
     c = conn.cursor()
     c.execute(f"SELECT * FROM {basename} ORDER BY likes DESC LIMIT 1")
@@ -81,8 +97,9 @@ def get_best(basename="mytable"):
     conn.close()
     return Joke(id=row[0], text=row[1], likes=row[2], dislikes=row[3])
 
+
 def get_worst(basename="mytable"):
-    conn = sqlite3.connect('data/jokes.db')
+    conn = sqlite3.connect("data/jokes.db")
 
     c = conn.cursor()
     c.execute(f"SELECT * FROM {basename} ORDER BY dislikes DESC LIMIT 1")
@@ -90,19 +107,21 @@ def get_worst(basename="mytable"):
 
     conn.close()
     return Joke(id=row[0], text=row[1], likes=row[2], dislikes=row[3])
-    
-def update(id, delta): #delta = (delta_likes, delta_dislikes)
+
+
+def update(id, delta):  # delta = (delta_likes, delta_dislikes)
     joke = get(id)
     joke.likes += delta[0]
     joke.dislikes += delta[1]
     add(joke)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     add(Joke(200, "90 10", 90, 10))
     add(Joke(201, "80 10", 80, 102))
     add(Joke(202, "100 10", 100, 10))
     add(Joke(100, "45 8", 45, 8))
-    
+
     print(get_best())
     print(get_worst())
     clear_database()
